3fa2ac9dc0fd4be365b307b2b18b7cb0925933f1
code-guy21 <alexissj16@knights.ucf.edu>
2018-04-18 17:08:46 -0400

    Add Xylophone Project

---
+++ Xylophone-iOS11/Xylophone/ VIewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Alexis San Javier on 4/18/18.
+//  Copyright © 2018 The Mexineer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        print(sender.tag)
+        
+    }
+    
+  
+
+}
+
