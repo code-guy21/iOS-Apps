524593c5cd2284102eda9ad94cc414fe3b9ce4aa
code-guy21 <alexissj16@knights.ucf.edu>
2018-04-20 01:21:52 -0400

    changes

--- Quizzler-iOS11/Quizzler/ViewController.swift
+++ Quizzler-iOS11/Quizzler/ViewController.swift
@@ -12,40 +12,82 @@ class ViewController: UIViewController {
     
     //Place your instance variables here
     
-    
+
+    @IBOutlet weak var startOverButton: UIButton!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
+    var bank = QuestionBank()
+    var question = 0
+    var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        questionLabel.text = bank.list[question].questionText
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(question+1)/13"
+        startOverButton.isHidden = true
+    }
+
+
+    @IBAction func answerPressed(_ sender: UIButton) {
         
-    }
-
-
-    @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag != 3 && question < 13{
+            updateUI(checkAnswer(sender.tag))
+            nextQuestion()
+        }else if sender.tag == 3{
+            startOver()
+        }
     }
     
-    
-    func updateUI() {
-      
+    func updateUI(_ result : Bool) {
+        if result == true{
+            score += 1
+            scoreLabel.text = "Score: \(score)"
+        }
     }
     
 
     func nextQuestion() {
         
-    }
-    
-    
-    func checkAnswer() {
+        question += 1
+        if question < 13{
+            questionLabel.text = bank.list[question].questionText
+            progressLabel.text = "\(question+1)/13"
+            
+        }else{
+            startOverButton.isHidden = false
+        }
         
     }
     
     
+    func checkAnswer(_ answer : Int) -> Bool {
+        
+        var correct = true
+        
+        if answer == 2{
+            correct = false
+        } else if answer == 1{
+            correct = true
+        }
+        
+        if correct == bank.list[question].answer{
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    
     func startOver() {
-       
+        question = 0
+        score = 0
+        questionLabel.text = bank.list[question].questionText
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(question+1)/13"
+        startOverButton.isHidden = true
     }
     
 
