ce012230c6f0cbbe5e9b91b48c8d26ae126d81de
code-guy21 <alexissj16@knights.ucf.edu>
2018-04-19 21:59:42 -0400

    changes

--- Xylophone-iOS11/Xylophone/ VIewController.swift
+++ Xylophone-iOS11/Xylophone/ VIewController.swift
@@ -9,50 +9,32 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate{
     
-    var player : AVAudioPlayer?
+    var audioPlayer : AVAudioPlayer!
     var url : URL?
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func playNote(_ note : Int){
+        
+        url = Bundle.main.url(forResource: "note\(note)", withExtension: "wav")!
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+        } catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
+    }
 
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        if sender.tag == 1{
-            url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
-        }
-        
-        if sender.tag == 2{
-            url = Bundle.main.url(forResource: "note2", withExtension: "wav")!
-        }
-        if sender.tag == 3{
-            url = Bundle.main.url(forResource: "note3", withExtension: "wav")!
-        }
-        if sender.tag == 4{
-            url = Bundle.main.url(forResource: "note4", withExtension: "wav")!
-        }
-        if sender.tag == 5{
-            url = Bundle.main.url(forResource: "note5", withExtension: "wav")!
-        }
-        if sender.tag == 6{
-            url = Bundle.main.url(forResource: "note6", withExtension: "wav")!
-        }
-        if sender.tag == 7{
-            url = Bundle.main.url(forResource: "note7", withExtension: "wav")!
-        }
-        
-        do {
-            player = try AVAudioPlayer(contentsOf: url!)
-            guard let player = player else { return }
-            player.prepareToPlay()
-            player.play()
-        } catch {
-            print("error loading file")
-            // couldn't load file :(
-        }
+        playNote(sender.tag)
     }
     
   
